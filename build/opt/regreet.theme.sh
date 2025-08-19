a011cbb1acfb93f69a6eb87a9665c80705326af0
Voidus X <voidus-git@mail.voidus.dev>
2025-08-18 17:09:01 -0700

    fix: missing unzipped extract

--- build/opt/regreet.theme.sh
+++ build/opt/regreet.theme.sh
@@ -8,7 +8,7 @@ shared="/usr/share"
 echo "Installing Regreet Theme."
 # Our configuration relies on a third party theme not in COPR or fedora packages.
 fetch https://github.com/dracula/gtk/archive/master.zip -O /tmp/regreet/theme.zip
-unzip -qq /tmp/regreet/theme.zip
+unzip -q /tmp/regreet/theme.zip -d /tmp/regreet/
 add "${shared}/themes/Dracula"
 mv /tmp/regreet/gtk-master ${shared}/themes/Dracula
 # It extracts under this name, unzip has no ability to respect naming.
